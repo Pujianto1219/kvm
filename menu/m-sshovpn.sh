3880ccfb77eb253aff0a83874947a7f2b8b1bf43
Pujianto1219 <yasril.pujianto38@smk.belajar.id>
2026-05-15 18:24:21 +0700

    Update m-sshovpn.sh

--- menu/m-sshovpn.sh
+++ menu/m-sshovpn.sh
@@ -356,48 +356,38 @@ END
 # FORMAT NOTIFIKASI TELEGRAM YANG SUDAH DISATUKAN
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 TEXT="
-◇━━━━━━━━━━━━━━━━━◇
-Trial SSH Premium Account
-◇━━━━━━━━━━━━━━━━━◇
-Username        :  <code>$Login</code>
-Password        :  <code>$Pass</code>
-Expired On      :  $timer Minutes
-◇━━━━━━━━━━━━━━━━━◇
-ISP             :  $ISP
-CITY            :  $CITY
-Host            :  <code>$domen</code>
-Login Limit     :  ${iplim} IP
-Port OpenSSH    :  22
-Port Dropbear   :  109, 143
-Port SSH WS     :  80, 7788, 8181, 8282
-Port SSH SSL WS :  443
-Port SSL/TLS    :  8443,8880
-Port OVPN WS SSL:  2086
-Port OVPN SSL   :  990
-Port OVPN TCP   :  1194
-Port OVPN UDP   :  2200
-Proxy Squid     :  3128
-BadVPN UDP      :  7100, 7300, 7300
-◇━━━━━━━━━━━━━━━━━◇
-SSH UDP VIRAL : <code>$domen:1-65535@$Login:$Pass</code>
-◇━━━━━━━━━━━━━━━━━◇
-HTTP COSTUM WS : <code>$domen:80@$Login:$Pass</code>
-◇━━━━━━━━━━━━━━━━━◇
-Host Slowdns    :  <code>$sldomain</code>
-Port Slowdns     :  80, 443, 53
-Pub Key          :  <code> $slkey</code>
-◇━━━━━━━━━━━━━━━━━◇
-Payload WS/WSS   :
+🧿───────────────────🧿            
+               ✨TRIAL SSH✨
+🔹 Informasi Akun Anda
+┌─────────────────────
+│Username   : <code>$Login</code>
+│Password   : <code>$Pass</code>
+│Provider   : $ISP
+│Country    : $CITY
+│Domain     : <code>$domen</code>
+│NSDomain   : <code>$sldomain</code>
+│Pub Key    : <code>$slkey</code>
+│Dropbear   : 109, 143
+│SSH WS     : 80, 7788, 8181, 8282
+│SSL/TLS    : 443, 8443, 8880
+│OpenVPN    : 990, 1194, 2200, 2086
+│UDP Custom : 1-65535
+│Proxy Squid: 3128
+│BadVPN UDP : 7100, 7300
+└─────────────────────
+🫧HTTP CUSTOM WS
+<code>$domen:80@$Login:$Pass</code>
+🧿───────────────────🧿
+🫧Payload WS/WSS: 
 <code>GET / HTTP/1.1[crlf]Host: [host][crlf]Connection: Upgrade[crlf]User-Agent: [ua][crlf]Upgrade: ws[crlf][crlf]</code>
-◇━━━━━━━━━━━━━━━━━◇
-OpenVPN SSL      :  https://$domain:81/ssl.ovpn
-OpenVPN TCP      :  https://$domain:81/tcp.ovpn
-OpenVPN UDP      :  https://$domain:81/udp.ovpn
-◇━━━━━━━━━━━━━━━━━◇
-Save Link Account: https://$domain:81/ssh-$Login.txt
-◇━━━━━━━━━━━━━━━━━◇
-$author
-◇━━━━━━━━━━━━━━━━━◇
+🧿───────────────────🧿
+🫧Save Account: 
+https://$domain:81/ssh-$Login.txt
+🧿───────────────────🧿
+🚀IP Limit: ${iplim} IP
+⏳Expired : $timer Menit
+🧿───────────────────🧿
+♨ᵗᵉʳⁱᵐᵃᵏᵃˢⁱʰ ᵗᵉˡᵃʰ ᵐᵉⁿᵍᵍᵘⁿᵃᵏᵃⁿ ˡᵃʸᵃⁿᵃⁿ ᵏᵃᵐⁱ♨
 "
 
 curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
